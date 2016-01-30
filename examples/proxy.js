011405cf1e7b3bd9b4f5fc7d98e676b7fc3ddc37
Andris Reinman <andris@kreata.ee>
2016-01-30 15:04:45 +0200

    v1.0.0

--- examples/proxy.js
+++ examples/proxy.js
@@ -1,18 +1,5 @@
-# proxy-test-server
+'use strict';
 
-Proxy server for testing proxied connections
-
-## Usage
-
-Install from npm
-
-    npm install proxy-test-server
-
-## Create HTTP proxy server
-
-HTTP proxy supports only CONNECT requests
-
-```javascript
 var HttpConnectProxy = require('../lib/server');
 var proxy = new HttpConnectProxy();
 
@@ -24,14 +11,5 @@ proxy.on('connect', function (port, host, socket) {
     var time = new Date().toISOString().substr(0, 19).replace('T', '');
     console.log('[%s] From %s to %s:%s', time, socket.remoteAddress, host, port);
 });
-```
 
-## Test Proxy
-
-Make a http request against proxy
-
-    curl -i -x http://127.0.0.1:9999 https://google.com
-
-## License
-
-**MIT**
+// curl -i -x http://127.0.0.1:9999 https://google.com
